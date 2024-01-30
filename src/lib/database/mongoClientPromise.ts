8ba604d1f71d190ec08d0cace47f87cc99ede74b
chardiri <102613917+chardiri@users.noreply.github.com>
2024-01-30 11:33:28 -0500

    Update mongoClientPromise.ts

--- src/lib/database/mongoClientPromise.ts
+++ src/lib/database/mongoClientPromise.ts
@@ -17,20 +17,9 @@ const options = {};
 let client: MongoClient;
 let mongoClientPromise: Promise<MongoClient>;
 
-if (process.env.ENVIRONMENT === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongomongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongomongoClientPromise = client.connect();
-  }
-  mongoClientPromise = global._mongomongoClientPromise;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  mongoClientPromise = client.connect();
-}
-
+client = new MongoClient(uri, options);
+mongoClientPromise = client.connect();
+console.log(mongoClientPromise)
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default mongoClientPromise;
